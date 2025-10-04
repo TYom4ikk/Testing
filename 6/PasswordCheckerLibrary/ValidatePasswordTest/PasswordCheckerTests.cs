cf95f692f584c40cf11a91c3b9cae07febf6176e
TYom4ikk <zhakubekov20032006@gmail.com>
2025-10-04 12:32:01 +0500

    Добавил практическую 6

---
+++ 6/PasswordCheckerLibrary/ValidatePasswordTest/PasswordCheckerTests.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PasswordCheckerLibrary;
+
+namespace ValidatePasswordTest
+{
+    [TestClass]
+    public class PasswordCheckerTests
+    {
+        [TestMethod]
+        public void Check_8Symbols_ReturnsTrue()
+        {
+            // Arrange.
+            string password = "ASqw12$$";
+            bool expected = true;
+
+            // Act.
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            // Assert.
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Check_4Symbols_ReturnsFalse()
+        {
+            // Arrange.
+            string password = "Aq1$";
+
+            // Act.
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            // Assert.
+            Assert.IsFalse(actual);
+        }
+        [TestMethod]
+        public void Check_WithoutDigit_ReturnsFalse()
+        {
+            // Arrange.
+            string password = "ASqw$$aa";
+
+            // Act.
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            // Assert.
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void Check_WithoutSpecialSymbol_ReturnsFalse()
+        {
+            // Arrange.
+            string password = "ASqw1234";
+
+            // Act.
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            // Assert.
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void Check_WithoutUppercase_ReturnsFalse()
+        {
+            // Arrange.
+            string password = "asqw12$$";
+
+            // Act.
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            // Assert.
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void Check_WithoutLowercase_ReturnsFalse()
+        {
+            // Arrange.
+            string password = "ASQW12$$";
+
+            // Act.
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            // Assert.
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void Check_TooShortPassword_ReturnsFalse()
+        {
+            // Arrange.
+            string password = "A1$bc";
+
+            // Act.
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            // Assert.
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void Check_TooLongPassword_ReturnsFalse()
+        {
+            // Arrange.
+            string password = "Aa1$" + new string('x', 20); // длина > 20
+
+            // Act.
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            // Assert.
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void Check_ValidComplexPassword_ReturnsTrue()
+        {
+            // Arrange.
+            string password = "QwEr12$#";
+
+            // Act.
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            // Assert.
+            Assert.IsTrue(actual);
+        }
+    }
+}
---
+++ 6/PasswordCheckerLibrary/ValidatePasswordTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ValidatePasswordTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ValidatePasswordTest")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("53f7a280-9137-4342-a0ec-debd746f2cbe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
